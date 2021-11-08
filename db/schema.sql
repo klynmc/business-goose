eb6a3ba65ed6cd55ef9f210a4a455d77a033f1b8
Leia <87383466+klynmc@users.noreply.github.com>
2021-11-08 15:51:52 -0600

    add files

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_db
+CREATE DATABASE employee_db
+
+USE employee_db
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Linda', 'Nail', 'Office Manager', 1), ('Bob', 'Sullivan', 'Director', 2), ('Carrie', 'Glass', 'Web Developer', 3), ('Pattie', 'Lovemore', 'Salesman', 4)
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Office Manager', 150000, 1), ('Director', 100000, 2), ('Web Developer', 80000, 3), ('Sales', 70000, 4)
+
+INSERT INTO department (name)
+VALUES ('Management'), ('HR'), ('Tech'), ('Sales')
---
+++ index.js
@@ -0,0 +1,60 @@
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
+
+const questions = [
+    {
+        type: 'list',
+        name: 'contents',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+];
+
+function init() {
+    inquirer.prompt(questions)
+      .then(function(data) {
+        console.log(data)
+    })
+}
+
+init();
